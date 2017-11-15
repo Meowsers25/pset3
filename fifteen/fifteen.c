ac1018695ecab7c0d9a92c5c56b4202b59735f5d
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-11-15 01:50:22 +0000

    finished init function

--- fifteen/fifteen.c
+++ fifteen/fifteen.c
@@ -157,7 +157,9 @@ void greet(void)
 void init(void)
 {
     // TODO
+
     int counter = (d * d) - 1;
+
     for (int i = 0; i < d; i++)
     {
         for (int j = 0; j < d; j++)
@@ -166,7 +168,14 @@ void init(void)
             counter--;
         }
     }
+    if (d % 2 == 0)
+        {
+            int x = board[d-1][d-2];
+            board[d-1][d-2] = board[d-1][d-3];
+            board[d-1][d-3] = x;
+        }
 }
+
 
 /**
  * Prints the board in its current state.
